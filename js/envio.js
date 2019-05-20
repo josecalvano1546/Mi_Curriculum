00e181ccb5c1e4acdb7f63052de5af9b4736297c
josecalvano1546 <38114264+josecalvano1546@users.noreply.github.com>
2019-05-19 21:49:32 -0300

    Update envio.js

--- js/envio.js
+++ js/envio.js
@@ -1,47 +1,12 @@
-// JavaScript Document
-// Ajax para el envio de correo, en la opcion SALUDAME.
-$(document).ready(function(){
 
-	$(".formulario-contacto").bind("submit", function(){
+$(".formulario-contacto").submit(function(e) {
+  e.preventDefault();
 
-		$.ajax({
-			// Obtiene los elementos del formulario.
-			type: $(this).attr("method"),
-			url: $(this).attr("action"),
-			data: $(this).serialize(),
-			success: function(){
-				//Alerta de exito, en el caso que el mensaje se haya enviado correctamente.
-				$("#alerta").removeClass("hide").addClass("alert-success");
-				$(".respuesta").html("Enviado!");
-				$(".mensaje-alerta").html(" El mensaje ah sido enviado correctamente.");
-		
-				
-
-			},
-			error: function(){
-				//Alerta de fallo en caso de que el mensaje no haya sido enviado correctamente.
-				$("#alerta").removeClass("hide").addClass("alert-danger");
-				$(".respuesta").html("Error al enviar!");
-				$(".mensaje-alerta").html(" No pudimos enviar tu mensaje, intenta nuevamente.");
-				
-				
-			}
-			
-			
-
-		});
-		
-		
-		return false;
-		
-	
-		
-	});
-	
-
-
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!");
+  });
 });
-
 	
 
 
